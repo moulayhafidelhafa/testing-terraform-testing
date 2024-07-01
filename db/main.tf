1d55ac02d9a822836dd148d9003cc011b4016efe
moulayhafidelhafa <167933385+moulayhafidelhafa@users.noreply.github.com>
2024-06-30 23:11:42 -0500

    edit main

--- db/main.tf
+++ db/main.tf
@@ -5,5 +5,5 @@
 
 resource "aws_db_subnet_group" "default" {
   name       = "advanced-main"
-  subnet_ids = [value = data.terraform_remote_state.vpc.outputs]
+  subnet_ids = [data.terraform_remote_state.vpc.outputs.private_subnets]
 }
